163fbef1d34828f9f6c2ff6791090af4e84f4eb7
irinoria <irinasidorowskaya@yandex.ru>
2024-04-08 12:30:56 +0300

    add docsting to str_func methods

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(str):
+    """
+    Return a string with all uppercase
+    """
     return str.upper()
+
+def str_tittle(str):
+    """
+    Return 'str' in tittle case.
+    """
+    return str.tittle()
